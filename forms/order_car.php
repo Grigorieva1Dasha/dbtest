c7180cb41fbe88dbe959c2aad1759b51e63e690e
Grigorieva1Dasha <32798782+Grigorieva1Dasha@users.noreply.github.com>
2020-01-22 21:38:25 +0800

    Update order_car.php

--- forms/order_car.php
+++ forms/order_car.php
@@ -4,12 +4,23 @@
 
   <select name="id_client" class="form-control">
     <?php
-    foreach ($clients as $client){
-      echo '<option value="' . $client['id_client'] . '"' . ($client['id_client'] === $orders_cars['id_client'] ? ' selected' : '') 
-      . '>';
-      echo $client['surname'].' '.$client['name'].' '.$client['patronymic'];
-      echo '</option>';
-      echo "\n";
+    if (empty($_GET['id_order_car'])) {
+      foreach ($clients as $client){
+        echo '<option value="' . $client['id_client'] . '"' //. ($client['id_client'] === $orders_cars['id_client'] ? ' selected' : '') 
+        . '>';
+        echo $client['surname'].' '.$client['name'].' '.$client['patronymic'];
+        echo '</option>';
+        echo "\n";
+      }
+    }
+    else {
+      foreach ($clients as $client){
+        echo '<option value="' . $client['id_client'] . '"' . ($client['id_client'] === $orders_cars['id_client'] ? ' selected' : '') 
+        . '>';
+        echo $client['surname'].' '.$client['name'].' '.$client['patronymic'];
+        echo '</option>';
+        echo "\n";
+      }
     }
     ?>
   </select><br>
@@ -17,31 +28,59 @@
 <label>Автомобиль</label><br>
   <select name="id_car" class="form-control">
     <?php
+    if (empty($_GET['id_order_car'])){
         foreach ($cars as $car) {
-            echo '<option value="' . $car['id_car'] . '"' . ($car['id_car'] === $orders_cars['id_car'] ? ' selected' : '') 
+            echo '<option value="' . $car['id_car'] . '"' //. ($car['id_car'] === $orders_cars['id_car'] ? ' selected' : '') 
             . '>';
             echo $car['id_brand'].' '.$car['price'];
             echo '</option>';
             echo "\n";
         }
+      }
+      else {
+        foreach ($cars as $car) {
+          echo '<option value="' . $car['id_car'] . '"' . ($car['id_car'] === $orders_cars['id_car'] ? ' selected' : '') 
+          . '>';
+          echo $car['id_brand'].' '.$car['price'];
+          echo '</option>';
+          echo "\n";
+      }
+      }
     ?>
   </select><br>
 
 <label>ФИО работника</label><br>
     <select name="id_worker" class="form-control">
     <?php
-    foreach ($workers as $worker){
-        echo '<option value="' . $worker['id_worker'] . '"' . ($worker['id_worker'] === $orders_cars['id_worker'] ? ' selected' : '') 
-        . '>';
+    if (empty($_GET['id_order_car'])){
+      foreach ($workers as $worker){
+        echo '<option value="' . $worker['id_worker'] . '">';
         echo $worker['surname'].' '.$worker['name'].' '.$worker['patronymic'];
         echo '</option>';
         echo "\n";
+      }
+    }
+    else{
+      foreach ($workers as $worker){
+          echo '<option value="' . $worker['id_worker'] . '"' . ($worker['id_worker'] === $orders_cars['id_worker'] ? ' selected' : '') 
+          . '>';
+          echo $worker['surname'].' '.$worker['name'].' '.$worker['patronymic'];
+          echo '</option>';
+          echo "\n";
+      }
     }
     ?>
 </select><br>
 
 <label>Статус</label><br>
-  <input class="form-control" type="text" name="status" value="<?= !empty($orders_cars['status']) ? $orders_cars['status'] : ""?>"><br>
+  <?php
+  if (empty($_GET['id_order_car'])){
+    echo '<input class="form-control" type="text" name="status"><br>';
+  }
+  else{
+    echo '<input class="form-control" type="text" name="status" value = "' . $orders_cars['status'].'"><br>';
+  }
+  ?>
   <br>
   <button class="btn btn-primary" type="submit">Сохранить</button>
   <button class="btn btn-primary" type="button" onclick="window.location='/index.php?function=orders_cars'">Назад</button>
